f6e2dc95d66b0085f23cbede1e823982ebd7c560
ACKSIL-droid <acbskinner@gmail.com>
2022-03-04 13:50:40 +0200

    Add base animations
    This commit adds the framework for the primary animation/gameplay loop.

--- script.js
+++ script.js
@@ -5,23 +5,38 @@ let i = 1;
 let computerHand;
 let playerHand;
 
-const buttons = document.querySelectorAll('.playerChoice');
-let roundResult = document.getElementById('roundResult');
-let gameResult = document.getElementById('gameResult');
+const gameSpace = document.getElementById('gamespace');
+    const announcer = document.getElementById('announcer');
+    const buttons = document.querySelectorAll('.playerchoice');
+    let disappear = document.querySelectorAll('.disappear');
+    let versus = document.getElementById('versus');
+    let computer = document.getElementById('computer');
 
+const resultSpace = document.getElementById('resultspace');
+resultSpace.style.display = "none";
+
+const footer = document.getElementById('footer');
+    const roundResult = document.getElementById('roundresult');
+    const scorer = document.getElementById('scorer');
+    const nextRound = document.getElementById('nextround');
+    nextRound.disabled = true;
 
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
         computerHand = computerPlay();
         playerHand = button.id;
+        
+        e.target.classList.add('visible');
+        e.target.classList.remove('zoom');
+        e.target.disabled = true;
 
         playRound(computerHand,playerHand);
+        animateRound();
         i++;
 
         if (computerWins == 5 || playerWins == 5) {
             winner();
-            gameReset();
         }
     }
 )});
@@ -30,31 +45,68 @@ buttons.forEach((button) => {
 function computerPlay() {
     let rng = Math.floor(Math.random()*3) + 1;
     let choice;
-    rng == 1 ? choice = 'rock' :
-    rng == 2 ? choice = 'paper':
-    choice = 'scissors';
+    switch (rng) {
+        case 1:
+            choice = 'rock';
+        break;
+        case 2:
+            choice = 'paper';
+        break;
+        case 3:
+            choice = 'scissors';
+    }
     return choice;
 }
-  
+
 
 function playRound() {
+    announcer.textContent = `Round ${i}!`;
     if (computerHand == playerHand) {
-        roundResult.textContent = `Round ${i}: Tie! You both selected ${computerHand}. Go again!`;   
+        roundResult.textContent = `Tie! You both selected ${computerHand}. Go again!`;   
     }
     else if ( (computerHand == 'rock' && playerHand == 'scissors') || (computerHand == 'scissors' && playerHand == 'paper') || (computerHand == 'paper' && playerHand == 'rock') ) {
-        roundResult.textContent = `Round ${i}: ${computerHand} beats ${playerHand}; computer wins!`;
+        roundResult.textContent = `${computerHand.charAt(0).toUpperCase() + computerHand.slice(1)} beats ${playerHand}; computer wins!`;
         computerWins++;
     }
     else {
-        roundResult.textContent = `Round ${i}: ${playerHand} beats ${computerHand}; player wins!`;
+        roundResult.textContent = `${playerHand.charAt(0).toUpperCase() + playerHand.slice(1)} beats ${computerHand}; player wins!`;
         playerWins++;
     }
-    gameResult.style.opacity = 1;
-    gameResult.textContent = `Computer: ${computerWins}\xa0\xa0\xa0Player: ${playerWins}`;
+    scorer.style.opacity = 1;
+    scorer.textContent = `Computer: ${computerWins}\xa0\xa0\xa0Player: ${playerWins}`;
 }
 
+function animateRound() {
+
+    buttons.forEach((button) => {
+        if (!button.classList.contains('visible')) {
+            button.classList.add('invisible');
+        }
+        //if (button.classList.contains('winner)) {change style, border, zoom}
+    });
+    versus.style.display = 'block';
+    computer.style.display = 'block';
+    nextRound.style.opacity = '1';
+    nextRound.disabled = false;
+    
+    switch (computerHand) {
+        case 'rock':
+            computer.src="./img/computer-rock.png";
+        break;
+        case 'paper':
+            computer.src="./img/computer-paper.png";
+        break;
+        case 'scissors':
+            computer.src="./img/computer-scissors.png";
+        break;
+    }
+}
 
 function winner() { 
+    gameSpace.style.display = 'none';
+    footer.style.display = 'none';
+    resultSpace.style.display = 'flex-column';
+
     if (computerWins > playerWins) {
         gameResult.textContent = `Computer wins the game with ${computerWins} points to ${playerWins}.`;
     }
